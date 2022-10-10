6d131b7dabc2cf1f3c32ccd20bfc8a74c89b9b19
DESKTOP-2TFB9TQ\Vladimir <vladimirbely983@gmail.com>
2022-10-10 04:54:27 +0300

    Added LS and some refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -6,26 +6,45 @@ const deleteButton = document.createElement('button');
 const deleteIcon = document.createElement('img');
 let inputValue;
 let markup;
+let LS = localStorage;
+let citiesArr = [];
 const apiKey = '818a8035fcf142c168c15d0f1d89529a';
 
 
-async function getFetchRequest() {
+async function getFetchData() {
     inputValue = searchInput.value;
-    if (!inputValue) {
+
+    if (!validateInput(inputValue)) return;
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`);
+    const data = await handleResponseError(response);
+    if (!data) return;
+
+    const { main, name, sys, weather } = data;
+    const cityObj = {
+        name,
+        country: sys.country,
+        temperature: Math.round(main.temp),
+        weather: weather[0]
+    };
+
+    console.log(cityObj);
+    return cityObj;
+}
+
+function validateInput(value) {
+    if (!value) {
         message.textContent = "Name of the city cannot be an empty string!";
         return;
     }
-    if (!isNaN(inputValue)) {
+    if (!isNaN(value)) {
         message.textContent = "Name of the city cannot be a number!";
         return;
     }
-
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`);
-    const data = await handelError(response);
-    return data;
+    return value;
 }
 
-function handelError(response) {
+function handleResponseError(response) {
     if (!response.ok) {
         message.textContent = "Please search for valid city";
         return;
@@ -33,24 +52,33 @@ function handelError(response) {
     return response.json();
 }
 
+function addCityToLS(data) {
+    if (!data) return;
+    citiesArr = JSON.parse(LS.getItem('Cities'));
+    if (citiesArr === null) citiesArr = [];
+    LS.setItem('city', JSON.stringify(data));
+    citiesArr.push(data);
+    LS.setItem('Cities', JSON.stringify(citiesArr));
+}
+
 function addCity(data) {
-    const { main, name, sys, weather } = data;
-    const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+    const icon = `https://openweathermap.org/img/wn/${data.weather["icon"]}@2x.png`;
     li = document.createElement("li");
     li.classList.add("city");
     markup = `
-            <h2 class="city-name" data-name="${name},${sys.country}">
-            <span>${name}</span>
-            <sup>${sys.country}</sup>
+            <h2 class="city-name">
+            <span>${data.name}</span>
+            <sup>${data.country}</sup>
             </h2>
-            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup>
+            <div class="city-temp">${data.temperature}<sup>°C</sup>
             </div>
             <figure>
-            <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-            <figcaption>${weather[0]["description"]}</figcaption>
+            <img class="city-icon" src=${icon} alt=${data.weather.main}>
+            <figcaption>${data.weather.description}</figcaption>
             </figure>`;
     li.innerHTML = markup;
     list.appendChild(li);
+    addCityToLS(data);
 }
 
 function clearForm() {
@@ -62,7 +90,7 @@ function clearForm() {
 }
 
 async function checkDublicates() {
-    let data = await getFetchRequest();
+    let data = await getFetchData();
     if (!data) return;
     const cityNames = list.querySelectorAll(' .city-name span');
     const cityNamesArr = Array.from(cityNames);
